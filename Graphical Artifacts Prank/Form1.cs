0b2d947ae6faad15ca4b1de9c55f24d5d1a6ac81
BuildTools <udenanton@hotmail.com>
2018-05-30 09:18:41 +0200

    using BackgroundWorker for main loop

--- Graphical Artifacts Prank/Form1.cs
+++ Graphical Artifacts Prank/Form1.cs
@@ -22,6 +22,7 @@ namespace Graphical_Artifacts_Prank
         Bitmap backBuffer;
         Graphics BB;
 
+        BackgroundWorker bw;
         bool isRunning;
 
         Random seed;
@@ -30,6 +31,7 @@ namespace Graphical_Artifacts_Prank
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            bw = new BackgroundWorker();
             this.Size = new Size(Screen.PrimaryScreen.Bounds.Width + 100, Screen.PrimaryScreen.Bounds.Height);
             this.Location = new Point(0, 0);
             this.BackColor = Color.Fuchsia;
@@ -46,10 +48,11 @@ namespace Graphical_Artifacts_Prank
 
             seed = new Random();
             isRunning = true;
-            startLoop();
+            bw.DoWork += startLoop;
+            bw.RunWorkerAsync();
         }
 
-        private void startLoop()
+        private void startLoop(object sender, EventArgs e)
         {
             while(isRunning)
             {
@@ -59,7 +62,7 @@ namespace Graphical_Artifacts_Prank
                     this.Focus();
                     Thread.Sleep(3);
                 }
-                catch(Exception e)
+                catch(Exception err)
                 {
 
                 }
@@ -68,39 +71,46 @@ namespace Graphical_Artifacts_Prank
 
         private Bitmap Render()
         {
-            BB.Clear(this.TransparencyKey);
-
-            int choice = seed.Next(0, 16);
-            //int choice = seed.Next(0, 150);
-
-            if (choice < 3)
+            try
             {
-                for(int i = 0; i < seed.Next(1, 30); i++)
+                BB.Clear(this.TransparencyKey);
+
+                int choice = seed.Next(0, 16);
+                //int choice = seed.Next(0, 150);
+
+                if (choice < 3)
                 {
-                    BB.FillRectangle(new SolidBrush(randomColor()), new Rectangle(seed.Next(0, this.Width), seed.Next(0, this.Height), seed.Next(2, 50), seed.Next(2, 50)));
+                    for (int i = 0; i < seed.Next(1, 30); i++)
+                    {
+                        BB.FillRectangle(new SolidBrush(randomColor()), new Rectangle(seed.Next(0, this.Width), seed.Next(0, this.Height), seed.Next(2, 50), seed.Next(2, 50)));
+                    }
+                }
+                if (choice > 6 && choice < 10)
+                {
+                    List<Point> starts = new List<Point>();
+                    List<Point> ends = new List<Point>();
+
+                    for (int n = 0; n < LinesAmount; n++)
+                    {
+                        int y = seed.Next(0, this.Height);
+
+                        starts.Add(new Point(0, y));
+                        ends.Add(new Point(this.Width, y));
+                    }
+
+                    foreach (Point start in starts)
+                    {
+                        BB.DrawLine(new Pen(new SolidBrush(randomColor())), start, ends[starts.IndexOf(start)]);
+                    }
+                }
+                if (choice == 10 || choice == 1 || choice == 2 || choice == 9)
+                {
+                    Thread.Sleep(seed.Next(10, 100));
                 }
             }
-            if(choice > 6 && choice < 10)
+            catch(Exception e)
             {
-                List<Point> starts = new List<Point>();
-                List<Point> ends = new List<Point>();
 
-                for(int n = 0; n < LinesAmount; n++)
-                {
-                    int y = seed.Next(0, this.Height);
-
-                    starts.Add(new Point(0, y));
-                    ends.Add(new Point(this.Width, y));
-                }
-
-                foreach (Point start in starts)
-                {
-                    BB.DrawLine(new Pen(new SolidBrush(randomColor())), start, ends[starts.IndexOf(start)]);
-                }
-            }
-            if (choice == 10 || choice == 1 || choice == 2 || choice == 9)
-            {
-                Thread.Sleep(seed.Next(10, 100));
             }
 
             return backBuffer;
